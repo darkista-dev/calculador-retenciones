4efcaab77cb926e290cdb36899d4dbf10a8df2dc
loualex128 <loualex128@gmail.com>
2023-09-25 20:48:32 -0400

    V1.3

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const iva = 0.16
+let moneda = "$"
 
 function obtenerValores(id) {
     let valor = parseFloat(document.getElementById(id).value);
@@ -13,11 +14,12 @@ function calcularRetencion() {
     let exento = obtenerValores("excent-field");
     let porcentaje = obtenerValores("rt-percent");
     let igtf = obtenerValores("igtf-percent");
+    moneda = document.getElementById("coin-selector").value;
 
     let total = (base + (base*iva) + exento);
     let pago = (base + ((base*iva)*porcentaje)) + exento;
     let pago_final = pago + (pago * igtf);
 
-    document.getElementById("total").innerHTML = total.toFixed(2) + "$";
-    document.getElementById("payment").innerHTML = pago_final.toFixed(2) + "$";
+    document.getElementById("total").innerHTML = total.toFixed(2) + " " + moneda;
+    document.getElementById("payment").innerHTML = pago_final.toFixed(2) + " " + moneda;
 }
